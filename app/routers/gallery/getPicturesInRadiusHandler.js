d83172d94fd52c046d7289ea0e6bd2a3281c62f0
Martin Ždila <m.zdila@gmail.com>
2017-06-12 21:46:44 +0200

    added more fields to gallrty item

--- app/routers/gallery/getPicturesInRadiusHandler.js
+++ app/routers/gallery/getPicturesInRadiusHandler.js
@@ -26,9 +26,10 @@ module.exports = function attachGetPicturesInRadiusHandler(router) {
       const lon2 = lon + distance / Math.abs(Math.cos(lat * Math.PI / 180) * 43);
 
       req.db.query(
-        `SELECT ImagePath, Title, Description, Lat, Lon,
-          (6371 * acos(cos(radians(?)) * cos(radians(Lat)) * cos(radians(Lon) - radians(?) ) + sin(radians(?)) * sin(radians(Lat)))) AS distance
-          FROM fm_Attachment
+        `SELECT RecordID, Created, ImagePath, Title, Description, Lat, Lon,
+          (6371 * acos(cos(radians(?)) * cos(radians(Lat)) * cos(radians(Lon) - radians(?) ) + sin(radians(?)) * sin(radians(Lat)))) AS distance,
+          nickname
+          FROM fm_Attachment JOIN fm_User ON UserID = user_id
           WHERE Lat BETWEEN ? AND ? AND Lon BETWEEN ? AND ?
           HAVING distance <= ?
           ORDER BY distance
@@ -39,8 +40,16 @@ module.exports = function attachGetPicturesInRadiusHandler(router) {
             logger.error({ err }, 'Error selecting pictures.');
             res.status(500).end();
           } else {
-            res.json(rows.map(({ ImagePath, Title, Description, Lat, Lon }) =>
-              ({ path: ImagePath, title: Title, description: Description, lat: Lat, lon: Lon })));
+            res.json(rows.map(({ RecordID, Created, ImagePath, Title, Description, Lat, Lon, nickname }) => ({
+              id: RecordID,
+              createdAt: Created.toISOString(),
+              path: ImagePath,
+              title: Title,
+              description: Description,
+              lat: Lat,
+              lon: Lon,
+              author: nickname,
+            })));
           }
         },
       );
