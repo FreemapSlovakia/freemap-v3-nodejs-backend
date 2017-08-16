f8b304fd230154e257b2c2402175565e0a891355
Martin Ždila <m.zdila@gmail.com>
2017-08-16 23:21:41 +0200

    added user comments to GET picture by id

--- app/routers/gallery/getPictureHandler.js
+++ app/routers/gallery/getPictureHandler.js
@@ -8,31 +8,49 @@ module.exports = function attachGetPictureHandler(router) {
     dbMiddleware,
     async (ctx) => {
       const rows = await ctx.state.db.query(
-        `SELECT picture.id AS pictureId, picture.createdAt, pathname, title, description, takenAt, picture.lat, picture.lon, user.id as userId, user.name,
+        `SELECT picture.id AS pictureId, picture.createdAt, pathname, title, description, takenAt, picture.lat, picture.lon,
+          user.id as userId, user.name,
           (SELECT GROUP_CONCAT(name SEPARATOR '\n') FROM pictureTag WHERE pictureId = picture.id) AS tags
         FROM picture LEFT JOIN user ON userId = user.id WHERE picture.id = ?`,
         [ctx.params.id],
       );
 
-      if (rows.length) {
-        const { pictureId, createdAt, title, description, takenAt, lat, lon, userId, name, tags } = rows[0];
-        ctx.body = {
-          id: pictureId,
-          createdAt: createdAt.toISOString(),
-          title,
-          description,
-          takenAt: takenAt ? takenAt.toISOString() : null,
-          lat,
-          lon,
-          user: userId && {
-            id: userId,
-            name,
-          },
-          tags: tags ? tags.split('\n') : [],
-        };
-      } else {
+      if (rows.length === 0) {
         ctx.status = 404;
       }
+
+      const commentRows = await ctx.state.db.query(
+        `SELECT pictureComment.id, pictureComment.createdAt, comment, user.name, userId
+          FROM pictureComment JOIN user ON (userId = user.id) WHERE pictureId = ? ORDER BY pictureComment.createdAt`,
+        [ctx.params.id],
+      );
+
+      const comments = commentRows.map(({ id, createdAt, comment, userId, name }) => ({
+        id,
+        createdAt: createdAt.toISOString(),
+        comment,
+        user: {
+          id: userId,
+          name,
+        },
+      }));
+
+      const { pictureId, createdAt, title, description, takenAt, lat, lon, userId, name, tags } = rows[0];
+      ctx.body = {
+        id: pictureId,
+        createdAt: createdAt.toISOString(),
+        title,
+        description,
+        takenAt: takenAt ? takenAt.toISOString() : null,
+        lat,
+        lon,
+        user: userId && {
+          id: userId,
+          name,
+        },
+        tags: tags ? tags.split('\n') : [],
+        comments,
+      };
     },
   );
 };
