b73f5f7d79627574f65ee6bf6afcf6eb41637c83
Martin Ždila <m.zdila@gmail.com>
2017-08-16 23:08:13 +0200

    added caching support for gallery image

--- app/routers/gallery/getPictureImageHandler.js
+++ app/routers/gallery/getPictureImageHandler.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
+const { promisify } = require('util');
+const calculate = require('etag');
 const { dbMiddleware } = require('~/database');
 const { acceptValidator } = require('~/requestValidators');
+
+const statSync = promisify(fs.stat);
 
 module.exports = function attachGetPictureHandler(router) {
   router.get(
@@ -13,11 +17,22 @@ module.exports = function attachGetPictureHandler(router) {
         [ctx.params.id],
       );
 
-      // TODO caching
+      const pathname = `${global.rootDir}/user_data/pictures/${rows[0].pathname}`;
 
       if (rows.length) {
+        const stats = await statSync(pathname);
+        ctx.status = 200;
+        ctx.response.lastModified = stats.mtime;
+        ctx.response.length = stats.size;
+        ctx.response.etag = calculate(stats, {
+          weak: true,
+        });
         ctx.type = 'image/jpeg';
-        ctx.body = fs.createReadStream(`${global.rootDir}/user_data/pictures/${rows[0].pathname}`);
+        if (ctx.fresh) {
+          ctx.status = 304;
+        } else {
+          ctx.body = fs.createReadStream(pathname);
+        }
       } else {
         ctx.status = 404;
       }
