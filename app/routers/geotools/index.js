29682542735b768ae1b10b2a35ff7af55b742631
Martin Ždila <m.zdila@gmail.com>
2018-09-08 19:56:27 +0200

    safer file handling

--- app/routers/geotools/index.js
+++ app/routers/geotools/index.js
@@ -14,7 +14,7 @@ const unlinkAsync = utils.promisify(fs.unlink);
 
 const router = new Router();
 
-// /elevation?coordinates=lat1,lon1,lat2,lon2
+// /elevation?coordinates=lat1,lon1,lat2,lon2,...
 router.get('/elevation', async (ctx) => {
   const { coordinates } = ctx.query;
   if (!coordinates) {
@@ -22,75 +22,36 @@ router.get('/elevation', async (ctx) => {
   }
 
   const fds = {};
+  try {
+    const items = await Promise.all(coordinates
+      .match(/[^,]+,[^,]+/g)
+      .map(pair => pair.split(',').map(c => Number.parseFloat(c)))
+      .map(async ([lat, lon]) => {
+        const alat = Math.abs(lat);
+        const alon = Math.abs(lon);
+        const key = `${lat >= 0 ? 'N' : 'E'}${Math.floor(alat + (lat < 0 ? 1 : 0)).toString().padStart(2, '0')}`
+        + `${lon >= 0 ? 'E' : 'W'}${Math.floor(alon + (lon < 0 ? 1 : 0)).toString().padStart(3, '0')}`;
 
-  const items = await Promise.all(coordinates
-    .match(/[^,]+,[^,]+/g)
-    .map(pair => pair.split(',').map(c => Number.parseFloat(c)))
-    .map(async ([lat, lon]) => {
-      const alat = Math.abs(lat);
-      const alon = Math.abs(lon);
-      const key = `${lat >= 0 ? 'N' : 'E'}${Math.floor(alat + (lat < 0 ? 1 : 0)).toString().padStart(2, '0')}`
-      + `${lon >= 0 ? 'E' : 'W'}${Math.floor(alon + (lon < 0 ? 1 : 0)).toString().padStart(3, '0')}`;
+        let fd = fds[key];
 
-      let fd = fds[key];
-
-      if (fd === undefined) {
-        const hgtPath = `${hgtDir}/${key}.HGT`;
-        try {
-          fd = await openAsync(hgtPath, 'r');
-        } catch (e) {
-          await fetchSafe(key);
-          fd = await openAsync(hgtPath, 'r');
+        if (fd === undefined) {
+          const hgtPath = `${hgtDir}/${key}.HGT`;
+          try {
+            fd = await openAsync(hgtPath, 'r');
+          } catch (e) {
+            await fetchSafe(key);
+            fd = await openAsync(hgtPath, 'r');
+          }
+          fds[key] = fd;
         }
-        fds[key] = fd;
-      }
 
-      return [lat, lon, fd];
-    }));
+        return [lat, lon, fd];
+      }));
 
-  ctx.response.body = await Promise.all(
-    items.map(async ([lat, lon, fd]) => {
-      const alat = Math.abs(lat);
-      const alon = Math.abs(lon);
-
-      const latFrac = alat - Math.floor(alat);
-      const lonFrac = alon - Math.floor(alon);
-      const y = (lat < 0 ? latFrac : (1 - latFrac)) * 3600;
-      const x = (lon < 0 ? (1 - lonFrac) : lonFrac) * 3600;
-
-      const x0 = Math.floor(x);
-      const y0 = Math.floor(y);
-      const wx0 = 1 - (x - x0);
-      const wy0 = 1 - (y - y0);
-      const x1 = Math.ceil(x);
-      const y1 = Math.ceil(y);
-      const wx1 = 1 - (x1 - x);
-      const wy1 = 1 - (y1 - y);
-
-      const buffer = Buffer.alloc(8);
-      await Promise.all([
-        readAsync(fd, buffer, 0, 2, (y0 * 3601 + x0) * 2),
-        readAsync(fd, buffer, 2, 2, (y1 * 3601 + x0) * 2),
-        readAsync(fd, buffer, 4, 2, (y0 * 3601 + x1) * 2),
-        readAsync(fd, buffer, 6, 2, (y1 * 3601 + x1) * 2),
-      ]);
-
-      const v00 = buffer.readInt16BE(0);
-      const v01 = buffer.readInt16BE(2);
-      const v10 = buffer.readInt16BE(4);
-      const v11 = buffer.readInt16BE(6);
-
-      return (0
-        + v00 * wx0 * wy0
-        + v01 * wx0 * wy1
-        + v10 * wx1 * wy0
-        + v11 * wx1 * wy1
-      ) / (wx0 * wy0 + wx0 * wy1 + wx1 * wy0 + wx1 * wy1);
-    }),
-  );
-
-  // TODO to finally
-  await Promise.all(Object.values(fds).map(fd => closeAsync(fd)));
+    ctx.response.body = await Promise.all(items.map(computeElevation));
+  } catch (e1) {
+    await Promise.all(Object.values(fds).map(fd => closeAsync(fd)));
+  }
 });
 
 module.exports = router;
@@ -135,4 +96,43 @@ async function fetch(key) {
   } catch (e) {
     await closeAsync(await openAsync(hgtPath, 'w+'));
   }
+}
+
+async function computeElevation([lat, lon, fd]) {
+  const alat = Math.abs(lat);
+  const alon = Math.abs(lon);
+
+  const latFrac = alat - Math.floor(alat);
+  const lonFrac = alon - Math.floor(alon);
+  const y = (lat < 0 ? latFrac : (1 - latFrac)) * 3600;
+  const x = (lon < 0 ? (1 - lonFrac) : lonFrac) * 3600;
+
+  const x0 = Math.floor(x);
+  const y0 = Math.floor(y);
+  const wx0 = 1 - (x - x0);
+  const wy0 = 1 - (y - y0);
+  const x1 = Math.ceil(x);
+  const y1 = Math.ceil(y);
+  const wx1 = 1 - (x1 - x);
+  const wy1 = 1 - (y1 - y);
+
+  const buffer = Buffer.alloc(8);
+  await Promise.all([
+    readAsync(fd, buffer, 0, 2, (y0 * 3601 + x0) * 2),
+    readAsync(fd, buffer, 2, 2, (y1 * 3601 + x0) * 2),
+    readAsync(fd, buffer, 4, 2, (y0 * 3601 + x1) * 2),
+    readAsync(fd, buffer, 6, 2, (y1 * 3601 + x1) * 2),
+  ]);
+
+  const v00 = buffer.readInt16BE(0);
+  const v01 = buffer.readInt16BE(2);
+  const v10 = buffer.readInt16BE(4);
+  const v11 = buffer.readInt16BE(6);
+
+  return (0
+    + v00 * wx0 * wy0
+    + v01 * wx0 * wy1
+    + v10 * wx1 * wy0
+    + v11 * wx1 * wy1
+  ) / (wx0 * wy0 + wx0 * wy1 + wx1 * wy0 + wx1 * wy1);
 }
